9d4136f6dbf6750b892147f68ee14e3887b8f135
𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬 <134224832+MD-JUBAED-AHMED-JOY@users.noreply.github.com>
2024-10-17 14:59:55 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -1,103 +1,60 @@
-const fs = require("fs-extra");
-const { utils } = global;
-
-module.exports = {
-	config: {
-		name: "prefix",
-		version: "1.4",
-		author: "NTKhang",
-		countDown: 5,
-		role: 0,
-		description: "Thay đổi dấu lệnh của bot trong box chat của bạn hoặc cả hệ thống bot (chỉ admin bot)",
-		category: "config",
-		guide: {
-			vi: "   {pn} <new prefix>: thay đổi prefix mới trong box chat của bạn"
-				+ "\n   Ví dụ:"
-				+ "\n    {pn} #"
-				+ "\n\n   {pn} <new prefix> -g: thay đổi prefix mới trong hệ thống bot (chỉ admin bot)"
-				+ "\n   Ví dụ:"
-				+ "\n    {pn} # -g"
-				+ "\n\n   {pn} reset: thay đổi prefix trong box chat của bạn về mặc định",
-			en: "   {pn} <new prefix>: change new prefix in your box chat"
-				+ "\n   Example:"
-				+ "\n    {pn} #"
-				+ "\n\n   {pn} <new prefix> -g: change new prefix in system bot (only admin bot)"
-				+ "\n   Example:"
-				+ "\n    {pn} # -g"
-				+ "\n\n   {pn} reset: change prefix in your box chat to default"
-		}
-	},
-
-	langs: {
-		vi: {
-			reset: "Đã reset prefix của bạn về mặc định: %1",
-			onlyAdmin: "Chỉ admin mới có thể thay đổi prefix hệ thống bot",
-			confirmGlobal: "Vui lòng thả cảm xúc bất kỳ vào tin nhắn này để xác nhận thay đổi prefix của toàn bộ hệ thống bot",
-			confirmThisThread: "Vui lòng thả cảm xúc bất kỳ vào tin nhắn này để xác nhận thay đổi prefix trong nhóm chat của bạn",
-			successGlobal: "Đã thay đổi prefix hệ thống bot thành: %1",
-			successThisThread: "Đã thay đổi prefix trong nhóm chat của bạn thành: %1",
-			myPrefix: "🌐 Prefix của hệ thống: %1\n🛸 Prefix của nhóm bạn: %2"
-		},
-		en: {
-			reset: "Your prefix has been reset to default: %1",
-			onlyAdmin: "Only admin can change prefix of system bot",
-			confirmGlobal: "Please react to this message to confirm change prefix of system bot",
-			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
-			successGlobal: "Changed prefix of system bot to: %1",
-			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "🌐 System prefix: %1\n🛸 Your box chat prefix: %2"
-		}
-	},
-
-	onStart: async function ({ message, role, args, commandName, event, threadsData, getLang }) {
-		if (!args[0])
-			return message.SyntaxError();
-
-		if (args[0] == 'reset') {
-			await threadsData.set(event.threadID, null, "data.prefix");
-			return message.reply(getLang("reset", global.GoatBot.config.prefix));
-		}
-
-		const newPrefix = args[0];
-		const formSet = {
-			commandName,
-			author: event.senderID,
-			newPrefix
-		};
-
-		if (args[1] === "-g")
-			if (role < 2)
-				return message.reply(getLang("onlyAdmin"));
-			else
-				formSet.setGlobal = true;
-		else
-			formSet.setGlobal = false;
-
-		return message.reply(args[1] === "-g" ? getLang("confirmGlobal") : getLang("confirmThisThread"), (err, info) => {
-			formSet.messageID = info.messageID;
-			global.GoatBot.onReaction.set(info.messageID, formSet);
-		});
-	},
-
-	onReaction: async function ({ message, threadsData, event, Reaction, getLang }) {
-		const { author, newPrefix, setGlobal } = Reaction;
-		if (event.userID !== author)
-			return;
-		if (setGlobal) {
-			global.GoatBot.config.prefix = newPrefix;
-			fs.writeFileSync(global.client.dirConfig, JSON.stringify(global.GoatBot.config, null, 2));
-			return message.reply(getLang("successGlobal", newPrefix));
-		}
-		else {
-			await threadsData.set(event.threadID, newPrefix, "data.prefix");
-			return message.reply(getLang("successThisThread", newPrefix));
-		}
-	},
-
-	onChat: async function ({ event, message, getLang }) {
-		if (event.body && event.body.toLowerCase() === "prefix")
-			return () => {
-				return message.reply(getLang("myPrefix", global.GoatBot.config.prefix, utils.getPrefix(event.threadID)));
-			};
-	}
+const dipto = require('axios');
+const fs = require('fs-extra');
+const path = require('path');
+const moment = require('moment-timezone');
+const pathFile = __dirname + '/cache/d1p.txt';
+if (!fs.existsSync(pathFile))
+  fs.writeFileSync(pathFile, 'true');
+  const isEnable = fs.readFileSync(pathFile, 'utf-8');
+module.exports.config = {
+  name: "prefix2",
+  version: "1.0.0",
+  hasPermission: 0,
+  credits: "diptoxRomim",
+  usePrefix: true,
+  description: "when send ,prefix then response",
+  commandCategory: "bot prefix",
+  usages: "prefix",
+  cooldowns: 5,
 };
+module.exports.handleEvent = async ({ api, event }) => {
+  if (isEnable == "true"){
+  const dipto2 = event.body ? event.body.toLowerCase() : '';
+   // const GP = "╭•┄┅════❁🌺❁════┅┄•╮\n${GP}\n╰•┄┅════❁🌺❁════┅┄•╯\n\n"; 
+  //HUSSAIN_BOT🤗🖤"
+     let d1PInfo = await api.getThreadInfo(event.threadID);
+  let diptoName = d1PInfo.threadName;
+    var time = moment.tz("Asia/Dhaka").format("LLLL");
+  const text = ` 𝙱𝙾𝚃 𝙽𝙰𝙼𝙴 ::${global.config.BOTNAME} 🤍\n \n: 𝙱𝙾𝚃 𝙿𝚁𝙴𝙵𝙸𝚇:${global.config.PREFIX} 💝\n\n: 𝙱𝙾𝚃 𝙲𝙾𝙼𝙼𝙰𝙽𝙳:${client.commands.size} ｣🖤\n\n𝚃𝙾𝙳𝙰𝚈 𝚃𝙸𝙼𝙴: ${time}\n𝚈𝙾𝚄𝚁 𝙱𝙾𝚇 𝙽𝙰𝙼𝙴: ${diptoName}💖\n\n♚════════════════════════♚\n\n𝗧𝗵𝗶𝘀 𝗕𝗼𝘁 𝗢𝘄𝗻𝗲𝗿: 𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬`
+  //const text2 = text[Math.floor(Math.random() * text.length)];
+const imgur = ["https://i.imgur.com/ndWGVUO.jpeg"]
+  const link = imgur[Math.floor(Math.random() * imgur.length)];
+  const res = await dipto.get(link, { responseType: 'arraybuffer' })
+const ex = path.extname(link);
+  const filename = __dirname + `/cache/Shaon${ex}`;
+  fs.writeFileSync(filename, Buffer.from(res.data, 'binary'));
+  if (dipto2.indexOf("prefix") ===0|| dipto2.indexOf("Prefix") ===0 )
+  {
+api.sendMessage({body:`${text}`,attachment: fs.createReadStream(filename)},event.threadID,() => fs.unlinkSync(filename),event.messageID)
+  }
+ }
+}
+module.exports.run = async ({api,args, event}) => {
+try {
+  if (args[0] == 'on') {
+    fs.writeFileSync(pathFile, 'true');
+    api.sendMessage('no prefix on successfully', event.threadID, event.messageID);
+  }
+  else if (args[0] == 'off') {
+    fs.writeFileSync(pathFile, 'false');
+    api.sendMessage('no prefix off successfully', event.threadID, event.messageID);
+  }
+  else if (!args[0]){
+    api.sendMessage(`Wrong format ${this.config.name}use off/on`, event.threadID, event.messageID);
+  }
+  }
+  catch(e) {
+    console.log(e);
+  }
+
+}

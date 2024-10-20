8c42e6f142cd3c2a73699db5631b25d966af312b
𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬 <134224832+MD-JUBAED-AHMED-JOY@users.noreply.github.com>
2024-10-20 11:01:56 +0600

    Update 😥.js

--- scripts/cmds/😥.js
+++ scripts/cmds/😥.js
@@ -1,22 +1,24 @@
-amr  module.exports = {
- config: {
-	 name: "😥.js",
-	 version: "1.0",
-	 author: "AceGun",
-	 countDown: 5,
-	 role: 0,
-	 shortDescription: "no prefix",
-	 longDescription: "no prefix",
-	 category: "no prefix",
- },
+ module.exports = {
+  config: {
+    name: "😥",
+    aliases: ["lm10"],
+    version: "1.0",
+    author: "Otineyyyy your dadddy👾😉",
+    countDown: 5,
+    role: 0,
+    shortDescription: "send you pic of messi",
+    longDescription: "",
+    category: "football",
+    guide: "{pn}"
+  },
 
- onStart: async function(){}, 
- onChat: async function({ event, message, getLang }) {
- if (event.body && event.body.toLowerCase() === "😥") {
- return message.reply({
- body: "࿐❛❛আঁগঁলে্ঁ ❛রাঁখঁতে্ঁ❜❜\n❛❛শিখো্ঁ ❛প্রি্ঁয়্ঁ❜❜-🥀😍࿐\n࿐❛❛😌কাঁরঁন্ঁ❜❛হাঁরিঁয়েঁ ❛ফেঁলাঁ ❛সঁহঁজঁ❜❜🥀😇࿐\n࿐❛❛😤কিঁন্তু্ঁ ❛ ফিঁরেঁ পাঁওঁয়াঁ ❜❜\n❛❛খুঁবঁইঁ ❛ কঁঠিঁন্ঁ❜❜🥀😘\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬",
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/JXjIRD5.mp4")
- });
- }
- }
+  onStart: async function ({ message }) {
+   var link = [ "https://i.imgur.com/UWtND17.mp4",
+]
+
+let img = link[Math.floor(Math.random()*link.length)]
+message.send({
+  body: '「 ࿐❛❛আঁগঁলে্ঁ ❛রাঁখঁতে্ঁ❜❜\n❛❛শিখো্ঁ ❛প্রি্ঁয়্ঁ❜❜-🥀😍࿐\n࿐❛❛😌কাঁরঁন্ঁ❜❛হাঁরিঁয়েঁ ❛ফেঁলাঁ ❛সঁহঁজঁ❜❜🥀😇࿐\n࿐❛❛😤কিঁন্তু্ঁ ❛ ফিঁরেঁ পাঁওঁয়াঁ ❜❜\n❛❛খুঁবঁইঁ ❛ কঁঠিঁন্ঁ❜❜🥀😘\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬」',attachment: await global.utils.getStreamFromURL(img)
+})
 }
+     }
